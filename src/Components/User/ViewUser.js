31c2c213346e733dd491e824b79dfa20916c924b
kinshuk-code-1729 <kinshuk2002.king@gmail.com>
2023-10-03 01:26:38 +0530

    oct commit 2

--- src/Components/User/ViewUser.js
+++ src/Components/User/ViewUser.js
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
 
 const ViewUser = () => {
+
+  const { userId } =useParams();
+  const initialState = { name: "", username: "", email: "", phone: "",website: ""};
+  const [user,setUser] = useState(initialState)
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+  
+  const fetchUser = async () => {
+      const response = await axios.get(`http://localhost:5000/users/${userId}`);
+      // console.log(response);
+      setUser(response.data);
+  }
+
   return (
     <div>ViewUser</div>
   )
